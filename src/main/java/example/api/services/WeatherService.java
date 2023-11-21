8a4835c26c0162bbbe75a0cca2f8c9ba12602750
adamczyk <lu.adamczyk97@gmail.com>
2023-11-21 18:16:43 +0100

    add function

--- src/main/java/example/api/services/WeatherService.java
+++ src/main/java/example/api/services/WeatherService.java
@@ -8,6 +8,8 @@ public class WeatherService {
     public CityOwResponse getWeatherFromOpenWeather() {
         String openWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?appid=716238e6166ce6e1315daf3232959cd3&q=Warsaw&units=metric";
 
+
+
         var httpClientService = new HttpClientService<CityOwResponse>();
         final CityOwResponse response = httpClientService.getWeather(openWeatherUrl, CityOwResponse.class);
 
